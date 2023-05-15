da2fdea4a25d82dcc4a6e4ee69c6ce9a03d35268
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-05-16 00:47:44 +0700

    Delete
    Xóa ghi chú

--- src/main/java/com/poly/controller/HomeController.java
+++ src/main/java/com/poly/controller/HomeController.java
@@ -4,13 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller // chi ra day la controller
+@Controller
 public class HomeController {
-	@GetMapping("/index") /*đánh dấu rằng phương thức được định nghĩa trong lớp này sẽ được sử dụng để
-							 * xử lý các request HTTP GET đến một đường dẫn cụ thể.
-							 */
-	public String index (Model model) {
-		model.addAttribute("message", "Welcome to Spring MVC");////lưu trữ các giá trị trong đối tượng request để chuyển dữ liệu đến index.jsp.
-		return "index";// trả về trang index.jsp
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("message", "Welcome to Spring MVC");
+		return "index";
 	}
 }
