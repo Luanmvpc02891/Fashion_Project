5240bb12965588c2fc5f13529d64465056cb9fe1
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-06-06 20:50:14 +0700

    Up

--- src/main/java/com/poly/controller/HomeController.java
+++ src/main/java/com/poly/controller/HomeController.java
@@ -1,29 +1,25 @@
 package com.poly.controller;
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import com.poly.entity.Category;
 import com.poly.entity.Product;
 import com.poly.entity.User;
-import com.poly.entity.cartDto;
+
 import com.poly.repository.CategoryRepository;
 import com.poly.repository.ProductRepository;
 import com.poly.repository.UserRepository;
 
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
+
 
 @Controller
 public class HomeController {
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<!-- Bài_2 -->
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Spring Boot</title>
+</head>
+<body>
+	<H1>Spring Home Page</H1>
+	<H2>${message}</H2>
+</body>
+</html>
